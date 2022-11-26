3f2b59d01d9cccdd9fd5e6e48618fac9fda0c96a
jvrvl <jvr.vll.l.bs.sa@gmail.com>
2022-11-26 15:18:39 -0300

    appRL

--- app/src/main/java/com/example/cajafuerte/Cambiouser.java
+++ app/src/main/java/com/example/cajafuerte/Cambiouser.java
@@ -2,63 +2,92 @@ package com.example.cajafuerte;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Cambiouser#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 public class Cambiouser extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://cajafuertefr-default-rtdb.firebaseio.com");
 
-    public Cambiouser() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Cambiouser.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Cambiouser newInstance(String param1, String param2) {
-        Cambiouser fragment = new Cambiouser();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cambiouser, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_cambiouser, container, false);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://cajafuertefr-default-rtdb.firebaseio.com");
+
+
+        final EditText contrasenia = v.findViewById(R.id.contraseniaAc);
+        final EditText cambiocu = v.findViewById(R.id.cambioCU);
+        final EditText cambiocun = v.findViewById(R.id.cambioCUN);
+
+        final Button btn = v.findViewById(R.id.cambioCUbtn);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String contraseniaT = contrasenia.getText().toString();
+                final String cambiocuT = cambiocu.getText().toString();
+                final String cambiocunT = cambiocun.getText().toString();
+
+                if (contraseniaT.isEmpty() || cambiocuT.isEmpty() || cambiocunT.isEmpty()){
+                    Toast.makeText(getContext(), "Por favor complete todos los campos", Toast.LENGTH_SHORT).show();
+                }
+                else if (contraseniaT.equals(cambiocunT)){
+                    Toast.makeText(getContext(), "La contraseña actual no coincide", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    databaseReference.child("Usuarios").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.hasChild(contraseniaT)){
+                                Toast.makeText(getContext(), "Contraseña ya registrada", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                databaseReference.child("Usuarios").child("pass").setValue(cambiocunT);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+            }
+        });
+
+
+
+        return v;
+
+
+
     }
 }
